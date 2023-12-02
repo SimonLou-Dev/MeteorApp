2e1ef01779750e84edab3ff7cb09807850318f33
Simon Lou <simon.lou.dev@proton.me>
2023-12-02 15:07:53 +0100

    Adding Json Weather decoder

---
+++ Utils/JsonResponses/CurrentWeatherJson.cs
@@ -0,0 +1,20 @@
+﻿namespace MeteorApp.Utils.JsonResponses;
+
+public class CurrentWeatherJson : BaseJson
+{
+
+    public current current { get; set; }
+
+}
+
+public class current
+{
+
+    public string time { get; set; }
+    public double temperature_2m { get; set; }
+    public double precipitation { get; set; }
+    public double wind_speed_10m { get; set; }
+    public double wind_direction_10m { get; set; }
+    public double interval { get; set; }
+
+}
